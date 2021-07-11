4fd0ecce68f9433420175059ff1aa4174b15ded9
jorgeluisRupay <jorgeluis17rh17@gmail.com>
2021-07-11 06:30:56 -0500

    Primer control

---
+++ js/app.js
@@ -0,0 +1,132 @@
+const ingresos =[
+    new Ingreso("Salario", 2100.00),
+    new Ingreso("Venta de coche",1500.00)
+];
+const egresos=[
+    new Egreso("Renta de apartamento",900.00),
+    new Egreso("Ropa",400.00)
+];
+
+let cargarApp =()=>{
+    cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
+}
+let totalIngresos =()=>{
+    let totalIngreso=0;
+    for(let ingreso of ingresos){
+        totalIngreso+=ingreso.valor;
+    }
+    return totalIngreso;
+}
+let totalEgresos=()=>{
+    let totalEgreso=0;
+    for(let egreso of egresos){
+        totalEgreso+=egreso.valor;
+    }
+    return totalEgreso;
+}
+//se encarga de actualizar el cabezero
+let cargarCabecero =()=>{
+    let presupuestoTotal = totalIngresos()-totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById("presupuesto").innerHTML = formatoMoneda(presupuestoTotal);
+    document.getElementById("porcentaje").innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById("ingresos").innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+}
+const formatoMoneda =(valor)=>{
+    return valor.toLocaleString("en-US",{style: 'currency', currency: 'USD', minimumFractionDigits: 2})
+}
+const formatoPorcentaje=(valor)=>{
+    return valor.toLocaleString("en-US",{style: 'percent', minimumFractionDigits: 2})
+}
+
+const cargarIngresos = ()=>{
+    let ingresosHTML = '';
+    for(let ingreso of ingresos){
+        ingresosHTML += crearIngresoHTML(ingreso);
+        //console.log(ingreso);
+    }
+    document.getElementById("lista-ingresos").innerHTML = ingresosHTML;
+}
+
+const crearIngresoHTML=(ingreso)=>{
+    let ingresoHTML = `<div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion"> ${ingreso.descripcion} </div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)} </div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="trash-outline" onclick='eliminarIngreso(${ingreso.idIngreso})'></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>
+`;
+return ingresoHTML;
+}
+const eliminarIngreso=(idIngreso)=>{
+    let indiceEliminar = ingresos.findIndex(ingreso=()=>{
+        ingreso.idIngreso === idIngreso;
+    });
+    ingresos.splice(indiceEliminar,1);
+    cargarCabecero();
+    cargarIngresos();
+    
+}
+
+const cargarEgresos=()=>{
+    let egresosHTML = '';
+    for(let egreso of egresos){
+        egresosHTML += crearEgresos(egreso);
+    }
+    document.getElementById("lista-egresos").innerHTML = egresosHTML;
+}
+const crearEgresos=(egreso)=>{
+    let egresoHTML = `<div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion"> ${egreso.descripcion} </div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor"> - ${formatoMoneda(egreso.valor)} </div>
+        <div class="elemento_porcentaje"> ${formatoPorcentaje(egreso.valor/totalEgresos())} </div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="trash-outline" onclick='eliminarEgreso(${egreso.idEgreso})'></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>
+    `;
+    return egresoHTML;
+}
+const eliminarEgreso=(idEgreso)=>{
+    let indiceEliminar = egresos.findIndex(egreso=()=>{
+        egreso.idEgreso === idEgreso;
+    });
+    egresos.splice(indiceEliminar,1);
+    cargarCabecero();
+    cargarEgresos();
+    
+}
+
+let agregarDato =()=>{
+    let forma = document.forms["forma"];
+    let tipo = forma["tipo"];
+    let descripcion = forma["descripcion"];
+    let valor = forma["valor"];
+    if(descripcion.value !== "" && valor.value !==""){
+        if(tipo.value === "ingreso"){
+            //+valor.value = se convierte en entero
+            //Number(valor.value)
+            ingresos.push(new Ingreso(descripcion.value, +valor.value));
+            cargarCabecero();
+            cargarIngresos();
+        }
+        else if(tipo.value==="egreso"){
+            egresos.push(new Egreso(descripcion.value, +valor.value));
+            cargarCabecero();
+            cargarEgresos();
+        }
+        document.getElementById("forma").reset();
+    }
+}
